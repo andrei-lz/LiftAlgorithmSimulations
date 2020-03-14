5413fe1ad708d3086f377bf05a76e3c59c7c274c
decallion <andrewstar999@yahoo.com>
2020-03-14 16:19:29 +0000

    different approach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 import time
-from objects import Lift, ElevatorShaft
+from objects import Elevator
 
 tk = Tk()
 
@@ -10,14 +10,8 @@ width = 800
 canvas = Canvas(tk, width = width, height = height)
 tk.title("Lift Simulation")
 canvas.pack()
-#l = canvas.create_rectangle(100, 100, 200, 200, fill = "black")
 
-objects = []
-
-system = ElevatorShaft(10, 100, 100, canvas, scale=0.5)
-l = system.lift
-
-objects.append(l)
+e = Elevator(10, 1000, 100, canvas)
 
 speed = 1
 while True:
--- objects.py
+++ objects.py
@@ -1,41 +1,8 @@
-class Lift:
+class Elevator:
     
-    def __init__(self, posX: int, posY: int, width: int, height: int, speed: int, canvas):
-        self.canvas = canvas
-        self.width = width
-        self.height = height
+    def __init__(self, storeys: int, posX: int, posY: int, canvas):
         self.posX = posX
         self.posY = posY
-        print(height, width)
-        self.speed = speed
-        self.currentStorey = 111
-        self.shape = canvas.create_rectangle(self.posX, self.posY, self.width, self.height, fill = "black")
-
-    def move_up(self):
-        currentPos = self.canvas.coords(self.shape)
-        self.canvas.move(self.shape, 0, -self.speed)
-
-    def move_down(self, storeys: int):
-        yDest = ((storeys - 1) * self.height) + self.posX
-        print("destination", yDest)
-        currentPos = self.canvas.coords(self.shape)
-        if (currentPos[3] != yDest):
-            self.canvas.move(self.shape, 0, self.speed)
-        return
-
-class ElevatorShaft:
-
-    def __init__(self, storeys: int, posX: int, posY: int, canvas, **kwargs):
-        self.storeys = storeys
-        self.scale = 1
-        for param, value in kwargs.items():
-            if param == "scale":
-                self.scale = value
-        
-        self.liftsize = 100 * self.scale
-        self.width = self.liftsize
-        self.height = self.liftsize * storeys
-        self.canvas = canvas
-        
-        self.lift = Lift(posY, posY, posX + self.liftsize, posY +self.liftsize, 1, self.canvas)
-        self.lift.canvas.create_rectangle(posX, posY, posX + self.width, posX + self.height)
+        self.width = 100
+        self.height = 50
+        self.storey_array = [canvas.create_rectangle(self.posX, self.posY+(self.height*i), self.posX+self.width, self.height + self.posY+(self.height*i), fill = "gray") for i in range(0, storeys)]
