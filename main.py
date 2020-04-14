e7b85e483e7d6e305917e9c4de3ed88e10135925
decallion <andrewstar999@yahoo.com>
2020-04-14 18:01:28 +0100

    mechanical elevator behaviour (without request update)

--- main.py
+++ main.py
@@ -12,11 +12,17 @@ def main():
     Sets up 2 elevators and some labels for the simulation
     Contains the main loop for TKinter graphical display
     '''
+    # Time settings for how long it takes to board a passenger or
+    # Move to a different floor in seconds
+    move_time = 10
+    board_time = 3
+    
+    # Setting up tkinter and a canvas for drawing
+
     tk = Tk()
 
     width = 1366
     height = 800
-
 
     canvas = Canvas(tk, width = width, height = height)
     tk.title("Lift Simulation")
@@ -30,12 +36,18 @@ def main():
     drawFloors(tk, e_m)
     e_m_title = Label(tk, text="Base Case Algorithm", font=("Impact", 24), justify=LEFT)
     e_m_title.place(x=e_m.posX, y=e_m.posY-e_m.height-25)
+    e_m_requests_left = 0
+    for index in requests_generated:
+        e_m_requests_left += len(index)
+    e_m_TOTAL_TIME = 0
 
     e_f = Elevator(10, 500, 100, canvas)
     e_f.lift.vel = 1
     drawFloors(tk, e_f)
     e_f_title = Label(tk, text="Improved Algorithm", font=("Impact", 24), justify=LEFT)
     e_f_title.place(x=e_f.posX, y=e_f.posY-e_f.height-25)
+    e_f_requests_left = e_m_requests_left # They start off with the same requests
+    e_f_TOTAL_TIME = 0
 
     while True:
         e_m.update()
@@ -45,8 +57,41 @@ def main():
 
         drawRequests(tk, e_f, requests_generated)
 
+        ### BEHAVIOURS ###
+
+        ## Mechanical Elevator
+
+        if e_m_requests_left > 0:
+            #Set the correct direction (only changes at the top or bottom)
+            e_m.lift.direction = "DOWN"
+            if e_m.lift.current_floor == e_m.storeys - 1:
+                e_m.lift.direction = "UP"
+                
+            # Board Passengers
+            if len(requests_generated[e_m.lift.current_floor]) > 0:
+                for passenger in requests_generated[e_m.lift.current_floor]:
+                    e_m.lift.addPassenger(passenger)
+                    e_m_requests_left -= 1
+                    e_m_TOTAL_TIME += board_time
+                    print(e_m.lift.current_floor, e_m.lift.passengers, e_m_requests_left, e_m_TOTAL_TIME)
+            
+            # Move the elevator up or down, however it's allowed
+            if e_m.lift.direction == "DOWN":
+                e_m.lift.current_floor += 1
+            else:
+                e_m.lift.current_floor -= 1
+            e_m_TOTAL_TIME += move_time
+
+            # Drop Passengers off at the floor they want
+            for passenger in e_m.lift.passengers:
+                if passenger == e_m.lift.current_floor:
+                    e_m.lift.removePassenger(passenger)
+                    e_m_TOTAL_TIME += board_time
+
+
+
         tk.update()
-        time.sleep(0.2)
+        time.sleep(.5)
 
 
 def drawRequests(tk: Tk, e_m: Elevator, requests_generated: list) -> None:
--- objects.py
+++ objects.py
@@ -4,25 +4,39 @@ Neeeded objects for the main module
 
 import tkinter as tk
 
+
 class Elevator:
-    
+
     def __init__(self, storeys: int, posX: int, posY: int, canvas):
         self.storeys = storeys
         self.posX = posX
         self.posY = posY
         self.width = 100
         self.height = 50
-        self.storey_array = [canvas.create_rectangle(self.posX, self.posY + self.height * i, self.posX + self.width, self.posY + self.height * (i+1), fill="gray", activefill="yellow", state=tk.DISABLED) for i in range(0, storeys)]
+        self.storey_array = [
+            canvas.create_rectangle(
+                self.posX,
+                self.posY + self.height * i,
+                self.posX + self.width,
+                self.posY + self.height * (
+                    i + 1),
+                fill="gray",
+                activefill="yellow",
+                state=tk.DISABLED) for i in range(
+                0,
+                storeys)]
         self.lift = Lift()
         self.canvas = canvas
-    
+
     def update(self):
         '''
         Called every tick, updates lift position
         '''
         for i in range(0, len(self.storey_array)):
-            self.canvas.itemconfig(self.storey_array[i], fill = "gray")
-        self.canvas.itemconfig(self.storey_array[self.lift.current_floor], fill="yellow")
+            self.canvas.itemconfig(self.storey_array[i], fill="gray")
+        self.canvas.itemconfig(
+            self.storey_array[self.lift.current_floor], fill="yellow")
+
 
 class Lift:
 
@@ -30,11 +44,11 @@ class Lift:
         self.capacity = 5
         self.current_floor = 0
         self.passengers = []
-    
+
     def addPassenger(self, passenger: int) -> None:
         self.passengers.append(passenger)
         self.capacity -= 1
-    
+
     def removePassenger(self, passenger: int) -> None:
         self.passengers.remove(passenger)
         self.capacity += 1
